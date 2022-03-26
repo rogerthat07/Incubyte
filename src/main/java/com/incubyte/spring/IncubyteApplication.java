b324b6ba9f30f2f81c740bf4216cc59c26daa7d5
unknown <singh.swaraj1999@gmail.com>
2022-03-27 02:33:00 +0530

    static data for first run

--- src/main/java/com/incubyte/spring/IncubyteApplication.java
+++ src/main/java/com/incubyte/spring/IncubyteApplication.java
@@ -1,13 +1,21 @@
 package com.incubyte.spring;
 
+import com.incubyte.spring.entity.Words;
+import com.incubyte.spring.repository.WordsRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class IncubyteApplication {
 
+	@Autowired
+	private static WordsRepository wr;
+
 	public static void main(String[] args) {
 		SpringApplication.run(IncubyteApplication.class, args);
+		wr.save(Words.builder().id(1L).word("word1").build());
 	}
 
 }
