678c6328759ecdd400e1709947212fe064ffbbc5
unknown <singh.swaraj1999@gmail.com>
2022-03-26 23:06:06 +0530

    initial commit

---
+++ src/main/java/com/incubyte/spring/IncubyteApplication.java
@@ -0,0 +1,13 @@
+package com.incubyte.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncubyteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncubyteApplication.class, args);
+	}
+
+}
